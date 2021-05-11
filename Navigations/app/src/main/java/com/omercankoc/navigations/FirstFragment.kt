b9416834c70765ad0fe8348aaeb77f254e85e7ea
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-05-11 16:56:14 +0300

    Complated Navigation Sample!

--- Navigations/app/src/main/java/com/omercankoc/navigations/FirstFragment.kt
+++ Navigations/app/src/main/java/com/omercankoc/navigations/FirstFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 class FirstFragment : Fragment() {
+
+    private lateinit var buttonNext : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +22,16 @@ class FirstFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        buttonNext = view.findViewById(R.id.buttonNext)
+        buttonNext.setOnClickListener {
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment("androidDeveloper")
+            Navigation.findNavController(it).navigate(action)
+        }
+
+        super.onViewCreated(view, savedInstanceState)
     }
 }
--- Navigations/app/src/main/java/com/omercankoc/navigations/SecondFragment.kt
+++ Navigations/app/src/main/java/com/omercankoc/navigations/SecondFragment.kt
@@ -5,8 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.Navigation
 
 class SecondFragment : Fragment() {
+
+    private lateinit var buttonBack : Button
+    private lateinit var textViewSecond : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +24,23 @@ class SecondFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        textViewSecond = view.findViewById(R.id.textViewSecond)
+
+        arguments?.let {
+            val username = SecondFragmentArgs.fromBundle(it).username
+            textViewSecond.text = username
+        }
+
+        buttonBack = view.findViewById(R.id.buttonBack)
+        buttonBack.setOnClickListener {
+            val action = SecondFragmentDirections.actionSecondFragmentToFirstFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+
+        super.onViewCreated(view, savedInstanceState)
     }
 }
